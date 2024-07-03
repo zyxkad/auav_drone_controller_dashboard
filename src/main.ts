a49f35a6ae9b42ebcff388ce448c0280c410554e
zyxkad <zyxkad@gmail.com>
2024-07-03 16:52:13 -0600

    set theme color to indigo

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+import { definePreset } from '@primevue/themes'
 import Aura from '@primevue/themes/aura'
 import FocusTrap from 'primevue/focustrap'
 import ToastService from 'primevue/toastservice'
@@ -16,10 +17,31 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+const primaryColor = 'indigo'
 app.use(PrimeVue, {
 	ripple: true,
 	theme: {
-		preset: Aura,
+		preset: definePreset(Aura, {
+			semantic: {
+				primary: {
+					50: `{${primaryColor}.50}`,
+					100: `{${primaryColor}.100}`,
+					200: `{${primaryColor}.200}`,
+					300: `{${primaryColor}.300}`,
+					400: `{${primaryColor}.400}`,
+					500: `{${primaryColor}.500}`,
+					600: `{${primaryColor}.600}`,
+					700: `{${primaryColor}.700}`,
+					800: `{${primaryColor}.800}`,
+					900: `{${primaryColor}.900}`,
+					950: `{${primaryColor}.950}`
+				},
+			},
+		}),
+		options: {
+			darkModeSelector: 'system',
+			cssLayer: false,
+		}
 	},
 })
 app.use(ToastService)
