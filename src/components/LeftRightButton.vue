2e07dab751c8bf0eb4f22f42c889d54e181871b9
zyxkad <zyxkad@gmail.com>
2024-07-06 22:18:22 -0600

    allow expand/shrink log box

---
+++ src/components/LeftRightButton.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps<{
+	disabledLeft?: boolean
+	disabledRight?: boolean
+}>()
+
+defineEmits<{
+	(e: 'clickLeft', event: PointerEvent): void
+	(e: 'clickRight', event: PointerEvent): void
+}>()
+
+import Button from 'primevue/button'
+import InputGroup from 'primevue/inputgroup'
+</script>
+
+<template>
+	<InputGroup>
+		<Button
+			v-show="!disabledLeft"
+			:style="{ width: disabledRight ? '100%' : '50%' }"
+			icon="pi pi-chevron-left"
+			severity="secondary"
+			@click="$emit('clickLeft', $event)"
+		/>
+		<Button
+			v-show="!disabledRight"
+			:style="{ width: disabledLeft ? '100%' : '50%' }"
+			icon="pi pi-chevron-right"
+			severity="secondary"
+			@click="$emit('clickRight', $event)"
+		/>
+	</InputGroup>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@ import DroneList from '@/components/DroneList.vue'
 import DroneOverview from '@/components/DroneOverview.vue'
 import LogBlock from '@/components/LogBlock.vue'
 import RtkStatus from '@/components/RtkStatus.vue'
+import LeftRightButton from '@/components/LeftRightButton.vue'
 import type { ColorInfo, DroneInfo, DroneStatusInfo, DronePositionInfo, LogMessage } from '@/api'
 import { DroneStatus } from '@/api'
 import * as api from '@/api/instance'
@@ -12,8 +13,57 @@ import { flightModeToString } from '@/data/flight_modes'
 
 const logBlk = ref<InstanceType<typeof LogBlock>>()
 
+const extLog = ref(0)
 const drones = reactive<Map<number, DroneInfo>>(new Map())
 const readonlyDrones = readonly(drones)
+
+const reverseLogBoxAni = ref(false)
+const extendingLog = ref(false)
+const shrinkingLog = ref(false)
+
+function extendLog(): void {
+	if (extLog.value === 0) {
+		if (!extendingLog.value) {
+			extendingLog.value = true
+			setTimeout(() => {
+				extendingLog.value = false
+				extLog.value = 1
+			}, 650)
+		}
+	} else if (extLog.value === -1) {
+		if (!shrinkingLog.value) {
+			reverseLogBoxAni.value = true
+			shrinkingLog.value = true
+			setTimeout(() => {
+				reverseLogBoxAni.value = false
+				shrinkingLog.value = false
+				extLog.value = 0
+			}, 500)
+		}
+	}
+}
+
+function shrinkLog(): void {
+	if (extLog.value === 0) {
+		if (!shrinkingLog.value) {
+			shrinkingLog.value = true
+			setTimeout(() => {
+				shrinkingLog.value = false
+				extLog.value = -1
+			}, 500)
+		}
+	} else if (extLog.value === 1) {
+		if (!extendingLog.value) {
+			reverseLogBoxAni.value = true
+			extendingLog.value = true
+			setTimeout(() => {
+				reverseLogBoxAni.value = false
+				extendingLog.value = false
+				extLog.value = 0
+			}, 650)
+		}
+	}
+}
 
 function onLedChanged(drone: number, color: ColorInfo) {
 	console.log('on led changed for:', drone, color)
@@ -61,8 +111,25 @@ onAwsEvent<DronePositionInfo>('drone-pos-info', ({ data }) => {
 </script>
 
 <template>
-	<main id="main">
-		<RtkStatus class="no-select rtk-status" />
+	<main
+		id="main"
+		:ext-log="extLog"
+		:class="{
+			'reverse-animation': reverseLogBoxAni,
+			'extenting-log-grid': extendingLog,
+			'shrinking-log-grid': shrinkingLog,
+		}"
+	>
+		<div class="rtk-status-wrapper">
+			<RtkStatus class="no-select rtk-status" />
+			<LeftRightButton
+				class="r-left-right-btn"
+				:disabledLeft="extLog === 1"
+				:disabledRight="extLog === -1"
+				@clickLeft="extendLog"
+				@clickRight="shrinkLog"
+			/>
+		</div>
 		<DroneOverview class="no-select drone-status" :drones="readonlyDrones" />
 		<DroneList class="drone-list" :drones="readonlyDrones" @ledChanged="onLedChanged" />
 		<LogBlock ref="logBlk" class="log-block" />
@@ -71,20 +138,87 @@ onAwsEvent<DronePositionInfo>('drone-pos-info', ({ data }) => {
 
 <style scoped>
 #main {
+	--ext-grid-width: 0rem;
+	--log-grid-width: 25rem;
 	display: grid;
 	height: calc(100vh - 5rem);
 	padding-bottom: 1rem;
 	grid-gap: 0.5rem;
 	grid-template:
-		'd r r' 10rem
-		'd l l' auto
-		's l l' 13rem
-		/ min(60%, 60rem) auto auto;
+		'd r' 10rem
+		'd l' auto
+		's l' 13rem
+		/ calc(100% - 0.5rem - var(--log-grid-width) + var(--ext-grid-width)) var(--log-grid-width);
+	overflow: visible;
 }
 
-.rtk-status {
+#main[ext-log='1'] {
+	--log-grid-width: 61.8%;
+}
+
+#main[ext-log='-1'] {
+	--ext-grid-width: 24rem;
+}
+
+@property --log-grid-width {
+	syntax: '<length> | <length-percentage>';
+	inherits: false;
+	initial-value: 0;
+}
+
+@property --ext-grid-width {
+	syntax: '<length> | <length-percentage>';
+	inherits: false;
+	initial-value: 0;
+}
+
+@keyframes extent-log-grid {
+	0% {
+		--log-grid-width: 25rem;
+	}
+	100% {
+		--log-grid-width: 61.8%;
+	}
+}
+
+@keyframes shrink-log-grid {
+	0% {
+		--ext-grid-width: 0;
+	}
+	100% {
+		--ext-grid-width: 24rem;
+	}
+}
+
+.extenting-log-grid {
+	animation: extent-log-grid 0.6s ease-out forwards;
+}
+
+.shrinking-log-grid {
+	animation: shrink-log-grid 0.45s ease-in forwards;
+}
+
+.reverse-animation {
+	animation-direction: reverse;
+}
+
+.rtk-status-wrapper {
+	position: relative;
 	grid-area: r;
 	height: 10rem;
+}
+
+.r-left-right-btn {
+	position: absolute;
+	top: -0.8rem;
+	left: -0.5rem;
+	width: 2.3rem;
+	height: 2.5rem;
+	display: none;
+}
+
+.rtk-status-wrapper:hover .r-left-right-btn {
+	display: block;
 }
 
 .drone-status {
